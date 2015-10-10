3b8b6eef0a8bce49476cac00ff8edbf453d7b3f0
Joe Reynolds <joereynolds952@googlemail.com>
2015-10-10 17:37:55 +0100

    create user json functiony goodness

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import package_manager
 import argparse
 import helpers
+import user
 
 def run():
     """Runs the whole shebang""" 
@@ -9,15 +10,25 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--install', '-i', help='installs a plugin to your plugin path')
     parser.add_argument('--remove', '-r', help='removes the specified plugin')
+    parser.add_argument('--setname','-sn', help='Sets the username in the configuration file.')
+    parser.add_argument('--setpath','-sp', help='Sets the plugin path for all downloaded packages to be moved to')
+    parser.add_argument('--setos','-so', help='Sets the operating system in the configuration file.')
     parser.add_argument('--view', help="View all packages")
     args = parser.parse_args()
     manager = package_manager.PackageManager()
+    reap_user = user.User()
 
+    print(reap_user.name)
+    print(reap_user.os)
+    print(reap_user.plugin_path)
     if args.install:
         manager.manage_packages(args.install)  
 
     if args.view :
         manager.show_packages()
+
+    if args.setname :
+        reap_user.set_name(args.setname)
 
 if __name__ == '__main__':
     run()
--- user.py
+++ user.py
@@ -1,7 +1,97 @@
+import json
+import sys
+import os
 
 class User():
 
     def __init__(self):
-        self.conf_file = open('user.conf')
+        self.conf_file = self.load_user_file()
+        self.plugin_path = self.get_plugin_path()
+        self.name = self.get_name()
+        self.os = self.get_os()
+
+    def load_user_file(self):
+        try : 
+            open('user.json')
+        except FileNotFoundError : 
+            print('User configuration file not found. Creating default.')
+            self.create_default_json()
+
+    def create_default_json(self):
+        """Creates a 'default' user if the user hasn't done any configuration yet"""
+        default_user = {"user" : 
+                           {
+                               "name" : os.environ['COMPUTERNAME'],
+                               "os" : sys.platform,
+                               "plugin-path" : self.create_default_plugin_path()
+                           }
+                       }
+
+        with open('user.json','w') as user_json:
+            json.dump(default_user, user_json)
+
+        self.plugin_path = default_user["user"]["plugin-path"]
+        self.name = default_user["user"]["name"]
+        self.os = default_user["user"]["os"]
+
+    def create_default_plugin_path(self):
+        """Creates a default plugin path that won't work. Forcing the user to specify one... bad idea or great idea?"""
+        return "plz."
+
+    def get_name(self):
+        """Returns the name field from the user.json file"""
+        pass
+ 
+    def get_os(self):
+        """Returns the os field from the user.json file"""
+        pass
+
+    def get_plugin_path(self):
+        """Returns the plugin-path field from the user.json file"""
+        pass
+
+
+    #Setting attributes is very similar. Can be refactored into one function probably
+    def set_name(self, name):
+        """Sets the name in the user.json file"""
+
+        user = {"user" : 
+                           {
+                               "name" : name,
+                               "os" : self.os,
+                               "plugin-path" : self.plugin_path
+                           }
+                       }
+
+        with open('user.json','w') as user_json:
+            json.dump(user, user_json)
+
+    def set_os(self, os):
+        """Sets the os in the user.json file"""
+ 
+        user = {"user" : 
+                           {
+                               "name" : self.name,
+                               "os" : os,
+                               "plugin-path" : self.plugin_path
+                           }
+                       }
+
+        with open('user.json','w') as user_json:
+            json.dump(user, user_json)
+
+    def set_plugin_path(self, path):
+        """Sets the plugin-path in the user.json file"""
+ 
+        user = {"user" : 
+                           {
+                               "name" : self.name,
+                               "os" : self.os,
+                               "plugin-path" : plugin_path
+                           }
+                       }
+
+        with open('user.json','w') as user_json:
+            json.dump(user, user_json)
 
 
