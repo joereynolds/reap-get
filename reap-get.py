96ddce966f28e568efa86f32ad489a9b3c7a033f
Joe Reynolds <joereynolds952@gmail.com>
2016-01-04 19:04:11 +0000

    add command to process reapfiles

--- reap-get.py
+++ reap-get.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import package_manager
-import argparse
 import user
+import argparse
+import package_manager
+
 
 def run():
     """Runs the whole shebang""" 
@@ -13,6 +14,7 @@ def run():
     parser.add_argument('--setname','-sn', help='Sets the username in the configuration file.')
     parser.add_argument('--setpath','-sp', help='Sets the plugin path for all downloaded packages to be moved to')
     parser.add_argument('--view-installed', '-vi', help='Displays installed plugins on the users machine')
+    parser.add_argument('--reap', '-rp',action='store_true', help='Installs the packages in reapfile.json')
     parser.add_argument('--setos','-so', help='Sets the operating system in the configuration file.')
     parser.add_argument('--getname','-gn', action='store_true', help='Gets the username in the configuration file.')
     parser.add_argument('--getpath','-gp', action='store_true', help='Gets the plugin path for all downloaded packages to be moved to')
@@ -28,6 +30,9 @@ def run():
     if args.install:
         manager.manage_packages(args.install)  
 
+    if args.reap:
+        manager.process_reapfile()
+
     if args.view:
         manager.show_packages()
 
@@ -38,7 +43,6 @@ def run():
 
     if args.remove:
         manager.remove_package(args.remove)
-        
 
     if args.getname:
         print(reap_user.name)
