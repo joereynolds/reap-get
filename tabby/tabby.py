3ab23f01cfeb5734c901f212d2d5aec4ca2150be
Joe Reynolds <joereynolds952@gmail.com>
2016-01-30 21:56:38 +0000

    docs and better formatting for tabby

--- tabby/tabby.py
+++ tabby/tabby.py
@@ -1,12 +1,34 @@
+"""
+Tabby is a ruff'n'ready printer for tabular data. Given data,
+it'll format it into a table for you
+
+An example call could be
+
+    names = ["Joe", "Sarah", "Jim", "Alice"]
+    ages  = [25, 32]
+    job   = ['Software Developer', 'Baker', 'Mechanical Engineer', 'Unemployed']
+    dob   = [1990, 1994, 1987, 1834]
+    height = ["5'6", '5 foot', '6 foot 3 inches']
+
+    tabby_print(names, ages, job, dob, height, headings=['name', 'ages', 'occupation','dob', 'height'])
+
+tabby_print takes an arbitrary amount of data and also an array of headings
+if you like.
+
+Note that the arrays passed in do not have to be equal length, any empty
+values will be filled with a null value
+
+Beware:
+"""
+
 import itertools
 
-"""Instead of using length like we do now, get the largest value for each
-column. That'll look better and hog less screen space"""
 def tabby_print(*arrays, **kwargs):
     """Prints it to the console"""
     zipped = itertools.zip_longest(*arrays, fillvalue="nil")
     length = max_all(*arrays)
-    divider = '+' + '-'*(length * len(arrays)) + '+'
+    l = get_table_length(*arrays)
+    divider = '+' + '-'*(l) + '+'
 
     for i, heading in enumerate(kwargs['headings']):
         l = get_column_length(i, itertools.zip_longest(*arrays, fillvalue='nil'))
@@ -31,6 +53,15 @@ def max_all(*arrays):
                _max = len(str(item))
     return _max
 
+def get_table_length(*arrays):
+    """Gets the sum of the max length of all columns
+    so that we know how long our table should be. Also
+    does +2 to the result so it looks nicer"""
+    _sum = 0
+    for i in range(len(arrays)):
+        _sum += get_column_length(i, itertools.zip_longest(*arrays, fillvalue='nil'))
+    return _sum + 2
+
 def get_column_length(n, zipped):
     """Returns the length of the largest string in a column.
     This is used to space our tables nicely. We +2 to the length
@@ -43,7 +74,7 @@ def get_column_length(n, zipped):
 def test():
     
     names = ["Joe", "Sarah", "Jim", "Alice", 'Melissa','Andrew']
-    ages  = [25, 32, 45, 30]
+    ages  = [25, 32, 45, 30,5 ,6]
     job   = ['Software Developer', 'Baker', 'Mechanical Engineer', 'Single Mother', 'Unemployed']
     dob   = [1990, 1994, 1987, 1834]
 
